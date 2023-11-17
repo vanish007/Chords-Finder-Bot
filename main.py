7d40c6a19508c186124dbc12894e2b737f8e9c3f
Иван Ерофеев <ivanerofeev01@gmail.com>
2023-11-17 17:20:00 +0300

    Edited main.py

--- main.py
+++ main.py
@@ -9,34 +9,24 @@ TELEGRAM_TOKEN = os.environ['TELEGRAM_BOT']
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['restart'])
 def send_welcome(message):
     bot.reply_to(message, "Список команд:\n"
-                          "/start - список команд\n"
-                          "/dice - случайное число от 1-6\n"
-                          # "/randint - случайное число из пользовательского диапозона\n"
+                          "/restart -обновить бота\n"
+                          "/dice - случайное предсказание\n"
                           "/donate - поддержать автора деньгами\n")
 
 
 @bot.message_handler(commands=['dice'])
 def send_welcome(message):
-    num = randint(1, 6)
-    bot.reply_to(message, str(num))
+    num = randint(0, 1)
+    wish = ["Сегодня вас ждет успех!", "Завтра вас собьет автобус"]
+    bot.reply_to(message, wish[num])
 
 
-# @bot.message_handler(commands=['randint'])
-# def send_welcome(message):
-# 	bot.reply_to(message, "Введите нижнюю границу: ")
-# 	low = int(input())
-# 	bot.reply_to(message, "Введите верхнюю границу: ")
-# 	high = int(input())
-# 	while high < low:
-# 		bot.reply_to(message, "Верхняя граница меньше нижней. \
-# 		Бот присылает случайное число из диапозона от нижней границы до верхней включительно, \
-# 		а поэтому верхняя граница должна быть больше нижней. Введите верхнюю границу еще раз: ")
-# 		high = int(input())
-# 	num = randint(low, high)
-# 	bot.reply_to(message, str(num))
+@bot.message_handler(commands=['donate'])
+def send_welcome(message):
+    bot.reply_to(message, "+7-(921)-777-77-77")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
