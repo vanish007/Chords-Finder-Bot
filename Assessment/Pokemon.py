bd3d4df97aa68bff0334b545b9990cc84bb450c9
Vanish007 <147030054+vanish007@users.noreply.github.com>
2024-04-16 21:58:07 +0300

    Update Pokemon.py
    Rewrite class using @dataclass

--- Assessment/Pokemon.py
+++ Assessment/Pokemon.py
@@ -1,19 +1,12 @@
+@dataclass(frozen=True, slots=True)
 class Pokemon(BasePokemon):
-    __id: int
-    __name: str
-    __height: int
-    __weight: int
-    __instances: List[Pokemon] = []
-
-    def __init__(self, id: int, name: str, height: int, weight: int) -> None:
-        self.__id = id
-        self.__name = name
-        self.__height = height
-        self.__weight = weight
-        self.__instances.append(self)
-
+    id: int
+    name: str
+    height: int
+    weight: int
+    
     def __str__(self) -> str:
-        return f'Pokemon ID is ' + color.BOLD + f'{self.__id}' + color.END + '\n' + \
-               f'Pokemon name is ' + color.BOLD + f'{self.__name}' + color.END + '\n' + \
-               f'Pokemon height is ' + color.BOLD + f'{self.__height}' + color.END + '\n' + \
-               f'Pokemon weight is ' + color.BOLD + f'{self.__weight}' + color.END
+        return f'Pokemon name is ' + color.BOLD + f'{self.name}' + color.END + '\n' + \
+               f'Pokemon ID is ' + color.BOLD + f'{self.id}' + color.END + '\n' + \
+               f'Pokemon height is ' + color.BOLD + f'{self.height}' + color.END + '\n' + \
+               f'Pokemon weight is ' + color.BOLD + f'{self.weight}' + color.END
