0fd507999b4229bb1c3325e8d0334eb1a174bc59
Vanish007 <147030054+vanish007@users.noreply.github.com>
2024-04-16 21:49:38 +0300

    Update main.py
    Edited some classes using @dataclass
    Added minor changes for better readability

--- Assessment/main.py
+++ Assessment/main.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from typing import Union, List, Dict, Generator
 import requests
+from dataclasses import dataclass
 
 
 cash: Dict[Union[str, int], Pokemon] = dict()
-
+BasePokemonList: List[BasePokemon] = []
+PokemonList: List[BasePokemon] = []
 
 class color:
    PURPLE = '\033[95m'
@@ -18,39 +20,25 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-
+@dataclass(frozen=True, slots=True)
 class BasePokemon:
-    __name: str
-    __instances: List[BasePokemon] = []
-
-    def __init__(self, name: str) -> None:
-        self.__name = name
-        self.__instances.append(self)
+    name: str
 
     def __str__(self) -> str:
-        return f'Pokemon name is ' + color.BOLD + f'{self.__name}' + color.END
+        return f'Pokemon name is ' + color.BOLD + f'{self.name}' + color.END
     
-
+@dataclass(frozen=True, slots=True)
 class Pokemon(BasePokemon):
-    __id: int
-    __name: str
-    __height: int
-    __weight: int
-    __instances: List[Pokemon] = []
-
-    def __init__(self, id: int, name: str, height: int, weight: int) -> None:
-        self.__id = id
-        self.__name = name
-        self.__height = height
-        self.__weight = weight
-        self.__instances.append(self)
-
+    id: int
+    name: str
+    height: int
+    weight: int
+    
     def __str__(self) -> str:
-        return f'Pokemon ID is ' + color.BOLD + f'{self.__id}' + color.END + '\n' + \
-               f'Pokemon name is ' + color.BOLD + f'{self.__name}' + color.END + '\n' + \
-               f'Pokemon height is ' + color.BOLD + f'{self.__height}' + color.END + '\n' + \
-               f'Pokemon weight is ' + color.BOLD + f'{self.__weight}' + color.END
- 
+        return f'Pokemon name is ' + color.BOLD + f'{self.name}' + color.END + '\n' + \
+               f'Pokemon ID is ' + color.BOLD + f'{self.id}' + color.END + '\n' + \
+               f'Pokemon height is ' + color.BOLD + f'{self.height}' + color.END + '\n' + \
+               f'Pokemon weight is ' + color.BOLD + f'{self.weight}' + color.END
 
 class PokeError(Exception):
     def __init__(self, *args, **kwargs):
@@ -60,6 +48,7 @@ class PokeError(Exception):
             self.message = kwargs[0]
         else:
             self.message = None
+            
     def __str__(self):
         if self.message:
             return f'{self.message}'
@@ -82,40 +71,43 @@ class PokeAPI:
                 name = pokemon_data['name']
                 height = pokemon_data['height']
                 weight = pokemon_data['weight']
-                BasePokemon(name)
-                Poke = Pokemon(id, name, height, weight)
+                BasePoke = BasePokemon(name)
+                Poke = Pokemon(id=id, name=name, height=height, weight=weight)
                 cash[name] = Poke
-                cash[int(id)] = Poke
-                cash[str(id)] = Poke
+                cash[id] = Poke
+                BasePokemonList.append(BasePoke)
+                PokemonList.append(Poke)
                 return Poke
     
     @staticmethod
     def get_all(get_full: bool = False) -> Generator[BasePokemon, Pokemon]:
         try:
             if get_full == True:
-                for data in Pokemon._Pokemon__instances:
+                for data in PokemonList:
                     yield data
             else:
-                for data in BasePokemon._BasePokemon__instances:
+                for data in BasePokemonList:
                     yield data
         except:
             raise PokeError
 
-# ## task 1 ##
-# ditto = PokeAPI.get_pokemon('ditto')
-# print(ditto)
+## task 1 ##
+ditto = PokeAPI.get_pokemon('ditto')
+print(ditto)
+print()
 
-# ## task 2 ##
-# mx_weight, second_mx_weight = 0, 0
-# mx_name, second_mx_name = '', ''
-# for i in range(1, 51):
-#     poke = PokeAPI.get_pokemon(i)
-#     poke_weight = poke._Pokemon__weight
-#     poke_name = poke._Pokemon__name
-#     if poke_weight >= mx_weight:
-#         second_mx_weight, second_mx_name = mx_weight, mx_name
-#         mx_weight, mx_name = poke_weight, poke_name
-#     elif poke_weight > second_mx_weight:
-#         second_mx_weight, second_mx_name = poke_weight, poke_name
-# print('The heaviest pokemon is ' + mx_name + ' with a total weight of ' + str(mx_weight) + '. '\
-#       'He is ' + str(mx_weight - second_mx_weight) + ' pokekilos heavier than second heaviest pokemon - ' + str(second_mx_name) + '.')
+## task 2 ##
+mx_weight, second_mx_weight = 0, 0
+mx_name, second_mx_name = '', ''
+for i in range(1, 51):
+    poke = PokeAPI.get_pokemon(i)
+    print(poke)
+    print()
+    if poke.weight >= mx_weight:
+        second_mx_weight, second_mx_name = mx_weight, mx_name
+        mx_weight, mx_name = poke.weight, poke.name
+    elif poke.weight > second_mx_weight:
+        second_mx_weight, second_mx_name = poke.weight, poke.name
+print('The heaviest pokemon is ' + mx_name + ' with a total weight of ' + str(mx_weight) + '. '\
+      'He is ' + str(mx_weight - second_mx_weight) + ' pokekilos heavier than second heaviest pokemon - ' + str(second_mx_name) + '.')
+print()
