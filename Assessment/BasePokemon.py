a40fff6543dfe1c0cd16b75db0608bc0a2ca37cb
Vanish007 <147030054+vanish007@users.noreply.github.com>
2024-04-16 21:59:40 +0300

    Update BasePokemon.py
    Rewrite class using @dataclass

--- Assessment/BasePokemon.py
+++ Assessment/BasePokemon.py
@@ -1,10 +1,6 @@
+@dataclass(frozen=True, slots=True)
 class BasePokemon:
-    __name: str
-    __instances: List[BasePokemon] = []
-
-    def __init__(self, name: str) -> None:
-        self.__name = name
-        self.__instances.append(self)
+    name: str
 
     def __str__(self) -> str:
-        return f'Pokemon name is ' + color.BOLD + f'{self.__name}' + color.END
+        return f'Pokemon name is ' + color.BOLD + f'{self.name}' + color.END
