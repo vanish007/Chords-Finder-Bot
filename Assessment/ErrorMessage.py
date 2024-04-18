be3c8aec1bc80b633de973c1bd415adf535c7c5f
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-04-18 09:19:32 +0300

    Changed return value to str

--- Assessment/ErrorMessage.py
+++ Assessment/ErrorMessage.py
@@ -1,13 +1,13 @@
 def ErrorMessage(num: int) -> str:
     if num == 1:
-        print(color.RED + color.BOLD + '####################################################')
-        print('#   Wrong input! Please enter number from 1 to 4   #')
-        print('####################################################' + color.END)
+        return color.RED + color.BOLD + '####################################################\n'+\
+        '#   Wrong input! Please enter number from 1 to 4   #\n' +\
+        '####################################################' + color.END
     elif num == 2:
-        print(color.RED + color.BOLD + '##########################################')
-        print('#   Wrong input! Please enter a number   #')
-        print('##########################################' + color.END)
+        return color.RED + color.BOLD + '##########################################\n'+\
+               '#   Wrong input! Please enter a number   #\n'+\
+              '##########################################' + color.END
     elif num == 3:
-        print(color.RED + color.BOLD + '#############################################')
-        print('#   Wrong input! Please enter digit 1 or 2  #')
-        print('#############################################' + color.END)
+        return color.RED + color.BOLD + '#############################################\n'+\
+               '#   Wrong input! Please enter digit 1 or 2  #\n'+\
+               '#############################################' + color.END
